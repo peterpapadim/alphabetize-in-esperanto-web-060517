222c133aa53eb5b5046a5dd57120b4694a0eed65
Panagiotis Papadimitropoulos <ppapadim12@gmail.com>
2017-05-31 00:24:52 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+
+  arr.sort_by do |word|
+    word.split("").collect do |letter|
+      esperanto.index(letter)
+  end
 end
+
+end
+
+
+alphabetize(["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"])
